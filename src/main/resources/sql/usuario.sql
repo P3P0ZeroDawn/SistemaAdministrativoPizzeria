3ee227571b3ba78d34d4355506efcc1a219fdcbd
emilianomb07 <moralesbaiza1@gmail.com>
2026-05-30 12:05:25 -0600

    Reemplazar script de inicialización de base de datos

--- src/main/resources/sql/usuario.sql
+++
@@ -1,8 +0,0 @@
-/**
- * Author:  pedro
- * Created: 25 may 2026
- */
-
-DROP USER IF EXISTS 'admin_pizzeria'@'%' identified by '12345';
-CREATE USER 'admin_pizzeria'@'%' identified by '12345';
-GRANT ALL PRIVILEGES ON pizzeria.* TO 'admin_pizzeria'@'%';
